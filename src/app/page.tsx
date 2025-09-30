5a707b2849464fc67e7440cdfa2e2770e564cc37
Pravin Malge <pravinmalge123@gmail.com>
2025-09-30 12:15:33 +0530

    tRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { prisma } from '@/lib/db'
 import React from 'react'
 
 const Page = async() => {
 
-  const users = await prisma.user.findMany();
-  console.log(users)
-
   return (
-    <div className='p-5'>
-     {JSON.stringify(users ,null,2)}
+    <div>
+      hello
     </div>
   )
 }
