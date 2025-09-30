79e1ae1758d12f1668d18d5b3203793928c836b0
Pravin Malge <pravinmalge123@gmail.com>
2025-09-30 13:46:26 +0530

    Ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { openai, createAgent } from "@inngest/agent-kit"
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+     const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summarize readable, concise, simple content.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+
+    const { output } = await summarizer.run(
+    `summerize the following text: ${event.data.text}`,
+    );
+
+    return {output};
   },
 );
