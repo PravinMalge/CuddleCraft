3c2f742cb6d14e2bc09a26eeda3fbbd7a51c3376
Pravin Malge <pravinmalge123@gmail.com>
2025-10-01 15:24:59 +0530

    e2b

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit"
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () =>{
+      const sandBox = await Sandbox.create("cuddlecraft-nextjs-test-2");
+      return sandBox.sandboxId;
+    })
      const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize readable, concise, simple content.",
@@ -16,6 +23,12 @@ export const helloWorld = inngest.createFunction(
     `summerize the following text: ${event.data.text}`,
     );
 
-    return {output};
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandBox = await getSandbox(sandboxId);
+        const host =  sandBox.getHost(3000);
+        return `https://${host}`
+    })
+
+    return {output , sandboxUrl };
   },
 );
